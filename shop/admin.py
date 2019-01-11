a8804c5016225ede9d0321346fdc5c8808f1a4e4
Egor Astapov <egorchles@yandex.ru>
2019-01-11 20:22:16 +0300

    video1: применение MPTT древовидной вложенности в админке 	изменено:      shop/admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-#from mptt.admin import MPTTModelAdmin
+from mptt.admin import MPTTModelAdmin
 
 from .models import Category, Product
 
 
-# class CategoryMPTTModelAdmin(MPTTModelAdmin):
-#     mptt_level_indent = 20
+class CategoryMPTTModelAdmin(MPTTModelAdmin):
+    mptt_level_indent = 20
 
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryMPTTModelAdmin)
 admin.site.register(Product)
