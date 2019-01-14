332acb6977eb45f043fd53a0f5db421425435dd4
Egor Astapov <egorchles@yandex.ru>
2019-01-14 18:34:59 +0300

    task2: выполнена запись в бд товара
    	изменено:      shop/views.py
    	изменено:      templates/shop/product-detail.html

--- shop/views.py
+++ shop/views.py
@@ -4,7 +4,8 @@ from django.views.generic.detail import DetailView
 from .form import *
 from django.views.generic.edit import FormMixin
 
-from .models import Product
+from .models import *
+from django.contrib.auth.models import User
 
 
 
@@ -23,5 +24,19 @@ class ProductDetail(FormMixin, DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form'] = self.get_form()
-        return context
 
+        """записываю данные в БД"""
+        if(self.request.GET):
+            quantity = self.request.GET['quantity']
+            slug = self.kwargs['slug']
+            # users = User.objects.values()
+            # cur_user = users[0].get('username')
+            # user = str(self.request.user)
+            # print(type(str(self.request.user)))
+            cart = Cart.objects.create(user=self.request.user)
+            product = Product.objects.filter(slug=slug)[0]
+            # product_id = Product.objects.filter(slug=slug)[0].id
+            CartItem.objects.create(product=product, quantity=quantity, cart=cart)
+            Orders.objects.create(cart=cart)
+
+        return context
