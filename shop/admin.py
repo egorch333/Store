282b507f8c603dfd6e2758308a2c5adbf3ebfc39
Egor Astapov <egorchles@yandex.ru>
2019-01-14 22:06:23 +0300

    task2: добавлены 3 модели (корзина, заказы, товары в корзине) в админку
    	изменено:      shop/admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,12 +1,31 @@
 from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
 
-from .models import Category, Product
+from .models import Category, Product, Cart, CartItem, Orders
 
 
 class CategoryMPTTModelAdmin(MPTTModelAdmin):
     mptt_level_indent = 20
 
 
+class CartAdmin(admin.ModelAdmin):
+    # вывод названия в таблице
+    list_display = ['user', 'accepted']
+
+
+class CartItemAdmin(admin.ModelAdmin):
+    # вывод названия в таблице
+    list_display = ['product', 'quantity']
+
+class OrdersAdmin(admin.ModelAdmin):
+    # вывод названия в таблице
+    list_display = ['cart', 'accepted']
+
+
+
+
 admin.site.register(Category, CategoryMPTTModelAdmin)
 admin.site.register(Product)
+admin.site.register(Cart, CartAdmin)
+admin.site.register(CartItem, CartItemAdmin)
+admin.site.register(Orders, OrdersAdmin)
