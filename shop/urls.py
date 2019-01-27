f12527afaa3fdb5d8b02bc321778e80dadc26fe9
Egor Astapov <egorchles@yandex.ru>
2019-01-27 21:59:59 +0300

    task6: выполнен поиск товаров
    	изменено:      shop/urls.py
    	изменено:      shop/views.py
    	изменено:      templates/shop/sidebar.html

--- shop/urls.py
+++ shop/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path("search/", views.Search.as_view(), name="search"),
     path("category/<slug:slug>/", views.CategoryProduct.as_view(), name="category"),
     path("checkout/<int:pk>/", views.CheckoutDetail.as_view(), name="checkout"),
+    path("sort/", views.SortProducts.as_view(), name="sort"),
 ]
 
