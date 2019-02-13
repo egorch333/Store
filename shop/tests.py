b6c3cccd9a33e0b32214223af71ef4b888e588ad
Egor Astapov <egorchles@yandex.ru>
2019-02-13 21:15:05 +0300

    усложнение тестирования. добавил еще данных. всего по 2 штуки
    	изменено:      shop/tests.py

--- shop/tests.py
+++ shop/tests.py
@@ -11,29 +11,35 @@ class SimpleTest(TestCase):
     def setUpTestData(cls):
         """наполнение тестовой базы данных
         без данных база пустая"""
-        Category.objects.create(name="Test", slug="test")
+        Category.objects.create(name="Test1", slug="test1")
+        Category.objects.create(name="Test2", slug="test2")
         Product.objects.create(
             category_id=1,
-            title="Product-test",
-            description="Desc",
-            slug="product-test")
+            title="Product-test1",
+            description="Desc1",
+            slug="product-test1")
+        Product.objects.create(
+            category_id=2,
+            title="Product-test2",
+            description="Desc2",
+            slug="product-test2")
 
     def test_category(self):
         """проверяю на точное совпадение строки"""
-        category = Category.objects.get(slug="test")
-        self.assertEqual(category.name, "Test")
+        category = Category.objects.get(slug="test1")
+        self.assertEqual(category.name, "Test1")
         print(1)
 
     def test_category_exists(self):
         """проверка на наличие записи в бд"""
-        category = Category.objects.filter(slug="test")
+        category = Category.objects.filter(slug="test1")
         self.assertTrue(category.exists())
         print(2)
 
     def test_my(self):
         """проверка подстроки в тексте"""
-        product = Product.objects.get(category__name__icontains="Test")
-        self.assertEqual(product.title, 'Product-test')
+        product = Product.objects.get(category__name__icontains="Test1")
+        self.assertEqual(product.title, 'Product-test1')
         print(3)
 
     def test_details(self):
@@ -45,7 +51,14 @@ class SimpleTest(TestCase):
 
     def test_cat_get(self):
         """вывод содержимого страницы"""
-        response = self.client.get('/category/test/')
+        response = self.client.get('/category/test1/')
         self.assertEqual(response.status_code, 200)
         # print(response.context["object_list"])
         print(5)
+
+    def test_cat_get(self):
+        """вывод количества объектов на странице"""
+        response = self.client.get('/')
+        # Check that the rendered context contains 5 customers.
+        self.assertEqual(len(response.context['object_list']), 2)
+        print(6)
